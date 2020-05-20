5f2a68fcb21a5a90e6584fce2a764793f64160cf
cg33 <chg80333@163.com>
2020-05-20 08:02:40 +0800

    fix(sword): sweetalert style

--- sword/resource/assets_list.go
+++ sword/resource/assets_list.go
@@ -1,7 +1,7 @@
 package resource
 
 var AssetsList = []string{
-	"/dist/css/all.min.a082fa9ab4.css",
+	"/dist/css/all.min.0a1ec34a6f.css",
 	"/dist/css/blue.png",
 	"/dist/css/blue@2x.png",
 	"/dist/css/fonts/6xK3dSBYKcSV-LCoeQqfX1RYOo3qOK7g.ttf",
