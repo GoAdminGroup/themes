dd7bdb3f17299b65b9fca6ca88abe5044c5b67d3
cg33 <chg80333@163.com>
2020-05-20 15:02:12 +0800

    feat(sword): improve swwtalert style

--- sword/resource/assets_list.go
+++ sword/resource/assets_list.go
@@ -1,7 +1,7 @@
 package resource
 
 var AssetsList = []string{
-	"/dist/css/all.min.0a1ec34a6f.css",
+	"/dist/css/all.min.59eacd3500.css",
 	"/dist/css/blue.png",
 	"/dist/css/blue@2x.png",
 	"/dist/css/fonts/6xK3dSBYKcSV-LCoeQqfX1RYOo3qOK7g.ttf",
--- sword/resource/assets_path.go
+++ sword/resource/assets_path.go
@@ -1,7 +1,7 @@
 package resource
 
 var AssetPaths = map[string]string{
-	"all.min.css":      "/dist/css/all.min.0a1ec34a6f.css",
+	"all.min.css":      "/dist/css/all.min.59eacd3500.css",
 	"all.min.js":       "/dist/js/all.min.8425540791.js",
 	"all_2.min.js":     "/dist/js/all_2.min.38a2a946b0.js",
 	"datatable.min.js": "/dist/js/datatable.min.581cdc109b.js",
