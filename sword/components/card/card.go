e183d979770c932c9b788e5b3765df29b7ad93ff
cg33 <chg80333@163.com>
2020-08-11 23:45:45 +0800

    feat(sword): update card

--- sword/components/card/card.go
+++ sword/components/card/card.go
@@ -3,6 +3,7 @@ package card
 import (
 	"html/template"
 
+	"github.com/GoAdminGroup/go-admin/modules/utils"
 	adminTemplate "github.com/GoAdminGroup/go-admin/template"
 	"github.com/GoAdminGroup/go-admin/template/types"
 )
@@ -15,14 +16,26 @@ type Card struct {
 	Content  template.HTML
 	Action   template.HTML
 	Footer   template.HTML
+
+	ID        string
+	BodyID    string
+	TopID     string
+	ContentID string
+	FooterID  string
 }
 
 func New() Card {
+	UUID := utils.Uuid(10)
 	return Card{
 		BaseComponent: &adminTemplate.BaseComponent{
 			Name:     "card",
 			HTMLData: List["card"],
 		},
+		ID:        UUID,
+		BodyID:    UUID + "_body",
+		TopID:     UUID + "_top",
+		ContentID: UUID + "_content",
+		FooterID:  UUID + "_footer",
 	}
 }
 
@@ -54,6 +67,31 @@ func (c Card) AddButton(button types.Button) Card {
 
 func (c Card) SetFooter(footer template.HTML) Card {
 	c.Footer = footer
+	return c
+}
+
+func (c Card) BindAction(action types.Action) Card {
+	c.BindActionTo(action, c.ID)
+	return c
+}
+
+func (c Card) BindActionToBody(action types.Action) Card {
+	c.BindActionTo(action, c.BodyID)
+	return c
+}
+
+func (c Card) BindActionToTop(action types.Action) Card {
+	c.BindActionTo(action, c.TopID)
+	return c
+}
+
+func (c Card) BindActionToContent(action types.Action) Card {
+	c.BindActionTo(action, c.ContentID)
+	return c
+}
+
+func (c Card) BindActionToFooter(action types.Action) Card {
+	c.BindActionTo(action, c.FooterID)
 	return c
 }
 
